bbf837a386f58654c901f000b00be9d0093e0ff4
itzzSVRtech <itzzsvrtech@itzzSVRtechs-MacBook-Air.local>
2026-04-01 20:21:28 +0530

    feat: Init Commit for 'MetricSub'.

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,8 @@
+import "@/global.css";
+import { Stack } from "expo-router";
+
+const AuthLayout = () => {
+    return <Stack screenOptions={{ headerShown: false }} />;
+};
+
+export default AuthLayout;
